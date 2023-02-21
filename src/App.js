68e3eb5f14bf81b87e57a868d9a337f9650fab78
Ivan <a1234@MacBook-Pro-1234.local>
2023-02-20 22:01:12 -0500

    3

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ function App() {
 	useEffect(() => {
 		tg.ready()
 	}, [])
-	const onClose = () => {
-		tg.close()
-	}
+	
 	return (
 		<div className="App">
 			привет!
 			<br />
 			<button className="body" onClick={onClose}> Push me! </button>
-			<button className="body" onClick={onClose}> Push me again! </button>
 		</div>
 	)
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Button = () => {
+	return <button {...props} className={'button' + props.className} />
+}
+
+export default Button
