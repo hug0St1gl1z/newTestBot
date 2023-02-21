750a5772813574e31e8809b6a54e8a7fbfa8c556
Ivan <a1234@MacBook-Pro-1234.local>
2023-02-20 22:33:52 -0500

    7

--- src/components/Hooks/useTelegram.jsx
+++ src/components/Hooks/useTelegram.jsx
@@ -1,18 +1,14 @@
 const tg = window.Telegram.WebApp
-export function useTelegram(){
+export function useTelegram() {
 	const onClose = () => {
 		tg.close()
 	}
-    const onToggleButton = () => {
-		if(tg.MainButton.isVisible){
-            tg.mainButton.hide()
-        } else {
-            tg.mainButton.show()}
+	const onToggleButton = () =>
+		tg.MainButton.isVisible ? tg.mainButton.hide() : tg.mainButton.show()
+	return {
+		onToggleButton,
+		onClose,
+		tg,
+		user: tg.initDataUnsafe?.user?.username,
 	}
-    return {
-        onToggleButton,
-        onClose,
-        tg,
-        user: tg.initDataUnsafe?.user?.username
-    }
 }
